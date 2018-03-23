2fc4f1835fec350055ce8814203a672ddee13087
JF <jfagudeloa@unal.edu.co>
2018-03-22 19:25:01 -0500

    'Good'

---
+++ app/router.js
@@ -0,0 +1,18 @@
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
+
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('about');
+  this.route('people', function() {
+    this.route('person', { path: ':id'});
+    this.route('workers');
+  });
+  this.route('workers');
+});
+
+export default Router;
